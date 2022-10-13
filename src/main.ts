6222a02358ae607f32372d65739dcb9180d52560
Prince Igwe <igwep297@gmail.com>
2022-10-13 03:27:16 +0100

    installed aws-sdk and multer for typescript

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {config as AWS_CONFIG} from 'aws-sdk'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // removes unnecessary properties in POST request body
       transform: true,
     }),
   );
+
+  AWS_CONFIG.update({
+    accessKeyId: process.env.FOOD_DEMO_S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.FOOD_DEMO_S3_SECRET_ACCESS_KEY
+  })
 
   await app.listen(3000);
 
